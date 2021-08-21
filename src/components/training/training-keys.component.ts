5f3155f587b8dcd58a671778e0e656f5ad8cb61a
ahenteti <ahmad.henteti@gmail.com>
2021-08-21 17:15:09 +0200

    improve training lesson styles

--- src/components/training/training-keys.component.ts
+++ src/components/training/training-keys.component.ts
@@ -3,13 +3,17 @@ import { BaseHtmlComponent } from '../_core/base-component';
 import { TextToTypeSubCategory } from '../../state/text-to-type-sub-category.enum';
 import { IAppStateClient } from '../../state/app-state.client.interface';
 import { AppStateClient } from '../../state/app-state.client';
-import { APP_SETTINGS_CHANGE_EVENT, TRAINING_LESSON_CHANGE_EVENT } from '../../constants/constant';
+import { END_TYPING_EVENT, MIN_STATS_TO_DISPLAY_PROGRESS_GRAPH, TRAINING_LESSON_CHANGE_EVENT } from '../../constants/constant';
 
 export class TrainingKeysHtmlComponent extends BaseHtmlComponent {
   private containerId: string;
   private container: HTMLElement;
 
-  constructor(private label: string, private keys: TextToTypeSubCategory, private appSettingsClient: IAppStateClient = AppStateClient.getInstance()) {
+  constructor(
+    private keysAyString: string,
+    private keys: TextToTypeSubCategory,
+    private appSettingsClient: IAppStateClient = AppStateClient.getInstance()
+  ) {
     super();
     this.containerId = this.generateId();
   }
@@ -19,7 +23,7 @@ export class TrainingKeysHtmlComponent extends BaseHtmlComponent {
   toHtml() {
     return /* html */ `
       <div id="${this.containerId}" class="training-keys-container">
-        <span class="keys">${this.label}</span>
+        <span class="keys">${this.keysAyString}</span>
         <span>Keys</span>
       </div>
     `;
@@ -29,6 +33,7 @@ export class TrainingKeysHtmlComponent extends BaseHtmlComponent {
     this.container = document.getElementById(this.containerId);
     this.update();
     this.addCustomEventListener(TRAINING_LESSON_CHANGE_EVENT, this.update.bind(this));
+    this.addCustomEventListener(END_TYPING_EVENT, this.update.bind(this));
     this.container.addEventListener('click', this.handleContainerClickEvent.bind(this));
   }
 
@@ -39,6 +44,42 @@ export class TrainingKeysHtmlComponent extends BaseHtmlComponent {
     } else {
       this.container.classList.remove('selected');
     }
+    const keyStats = this.appSettingsClient.getTypedKeysStatsMap();
+    if (keyStats) {
+      const speeds = this.keysAyString
+        .toLowerCase()
+        .split('')
+        .map((c) => {
+          if (keyStats.get(c)) {
+            let stats = keyStats.get(c);
+            let statsWpm = stats.filter((s) => s.wpm > 0).map((s) => s.wpm);
+            if (statsWpm.length >= MIN_STATS_TO_DISPLAY_PROGRESS_GRAPH) {
+              return this.arrayAvg(statsWpm);
+            }
+          }
+          return 0;
+        });
+      if (speeds.filter((s) => s == 0).length > 0) {
+        this.container.classList.remove('avg-wpm-lt-10', 'avg-wpm-lt-20', 'avg-wpm-lt-30', 'avg-wpm-lt-40', 'avg-wpm-lt-50', 'avg-wpm-gte-50');
+      } else if (speeds.filter((s) => s < 10).length > 0) {
+        this.container.classList.add('avg-wpm-lt-10');
+      } else if (speeds.filter((s) => s < 20).length > 0) {
+        this.container.classList.add('avg-wpm-lt-20');
+      } else if (speeds.filter((s) => s < 30).length > 0) {
+        this.container.classList.add('avg-wpm-lt-30');
+      } else if (speeds.filter((s) => s < 40).length > 0) {
+        this.container.classList.add('avg-wpm-lt-40');
+      } else if (speeds.filter((s) => s < 50).length > 0) {
+        this.container.classList.add('avg-wpm-lt-50');
+      } else {
+        this.container.classList.add('avg-wpm-gte-50');
+      }
+      this.container.title = `Typing speed: ${this.arrayAvg(speeds)}`;
+    }
+  }
+
+  private arrayAvg(array) {
+    return Math.round(array.reduce((sum, current) => sum + current, 0) / array.length);
   }
 
   private handleContainerClickEvent() {
--- src/components/typed-text-stats/typed-keys-stats.component.ts
+++ src/components/typed-text-stats/typed-keys-stats.component.ts
@@ -1,6 +1,7 @@
 import './typed-text-stats.scss';
 import { BaseHtmlComponent } from '../_core/base-component';
 import { IAppStateClient } from '../../state/app-state.client.interface';
+import { MIN_STATS_TO_DISPLAY_PROGRESS_GRAPH } from '../../constants/constant';
 
 export class TypedKeysStatsHtmlComponent extends BaseHtmlComponent {
   private typedKeysStatsId: string;
@@ -30,26 +31,28 @@ export class TypedKeysStatsHtmlComponent extends BaseHtmlComponent {
       if (keyStats && keyStats.get(c)) {
         let stats = keyStats.get(c);
         let statsWpm = stats.filter((s) => s.wpm > 0).map((s) => s.wpm);
-        let minWpm = Math.min(...statsWpm);
-        let maxWpm = Math.max(...statsWpm);
-        let avgWpm = Math.round(statsWpm.reduce((sum, current) => sum + current, 0) / statsWpm.length);
-        if (minWpm !== maxWpm) {
-          title = `Average typing speed: ${avgWpm}wpm&#10;Best typing speed: ${maxWpm}wpm`;
-        } else {
-          title = `Typing speed: ${avgWpm}`;
-        }
-        if (avgWpm < 10) {
-          cssClass = 'avg-wpm-lt-10';
-        } else if (avgWpm < 20) {
-          cssClass = 'avg-wpm-lt-20';
-        } else if (avgWpm < 30) {
-          cssClass = 'avg-wpm-lt-30';
-        } else if (avgWpm < 40) {
-          cssClass = 'avg-wpm-lt-40';
-        } else if (avgWpm < 50) {
-          cssClass = 'avg-wpm-lt-50';
-        } else {
-          cssClass = 'avg-wpm-gte-50';
+        if (statsWpm.length >= MIN_STATS_TO_DISPLAY_PROGRESS_GRAPH) {
+          let minWpm = Math.min(...statsWpm);
+          let maxWpm = Math.max(...statsWpm);
+          let avgWpm = Math.round(statsWpm.reduce((sum, current) => sum + current, 0) / statsWpm.length);
+          if (minWpm !== maxWpm) {
+            title = `Average typing speed: ${avgWpm}wpm&#10;Best typing speed: ${maxWpm}wpm`;
+          } else {
+            title = `Typing speed: ${avgWpm}`;
+          }
+          if (avgWpm < 10) {
+            cssClass = 'avg-wpm-lt-10';
+          } else if (avgWpm < 20) {
+            cssClass = 'avg-wpm-lt-20';
+          } else if (avgWpm < 30) {
+            cssClass = 'avg-wpm-lt-30';
+          } else if (avgWpm < 40) {
+            cssClass = 'avg-wpm-lt-40';
+          } else if (avgWpm < 50) {
+            cssClass = 'avg-wpm-lt-50';
+          } else {
+            cssClass = 'avg-wpm-gte-50';
+          }
         }
       }
       html += `<span title="${title}" class="typed-key ${cssClass}">${c.toUpperCase()}</span>`;
